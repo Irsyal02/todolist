2e0a23260266c6dd2c1a593fba7ef17a469b5419
Irsyal Rizky <irsyal02@gmail.com>
2025-07-09 20:16:37 +0700

    mengubah sedikit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,19 +67,20 @@
         accusamus impedit minima harum corporis iusto.
       </p>
 
+      
       <div class="mt-4 flex gap-4 sm:mt-6">
         <a
           class="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700"
-          href="#"
+          href="{{ route('login') }}"
         >
-          Get Started
+          Log in
         </a>
 
         <a
           class="inline-block rounded border border-gray-200 px-5 py-3 font-medium text-gray-700 shadow-sm transition-colors hover:bg-gray-50 hover:text-gray-900 dark:border-gray-700 dark:text-gray-200 dark:hover:bg-gray-800 dark:hover:text-white"
-          href="#"
+          href="{{ route('register') }}"
         >
-          Learn More
+          register
         </a>
       </div>
     </div>
