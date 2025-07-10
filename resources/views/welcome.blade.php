910028f3fd7d33a0c0010aa0382b0c72e181207f
Irsyal Rizky <irsyal02@gmail.com>
2025-07-10 18:05:38 +0700

    mengubah sedikit welcome blade php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,38 +20,56 @@
         @endif
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center max-h-screen flex-col">
-        <section class="bg-white lg:grid lg:h-screen lg:place-content-center dark:bg-gray-900">
+
+
+    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+
+    
+    
+    <section class="bg-white lg:grid lg:h-screen lg:place-content-center dark:bg-gray-900">
         <div
             class="mx-auto w-screen max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 md:grid md:grid-cols-2 md:items-center md:gap-4 lg:px-8 lg:py-32"
         >
             <div class="max-w-prose text-left">
             <h1 class="text-4xl font-bold text-gray-900 sm:text-5xl dark:text-white">
                 Tulis setiap ide yang ada di kepalamu dan
-                <strong class="text-indigo-600"> increase </strong>
-                conversions
+                <strong class="text-indigo-600"> todolist </strong>
+                di sini
             </h1>
 
             <p class="mt-4 text-base text-pretty text-gray-700 sm:text-lg/relaxed dark:text-gray-200">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, nisi. Natus, provident
-                accusamus impedit minima harum corporis iusto.
+                Sudah punya akun? <a href="{{ route('register') }}" class="text-indigo-600">Daftar sekarang</a>
             </p>
 
-            
-            <div class="mt-4 flex gap-4 sm:mt-6">
+    @if (Route::has('login'))
+                <div class="mt-4 flex gap-4 sm:mt-6">
+        @auth
+                <a
+                class="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700"
+                href="{{ url('/dashboard') }}"
+                >
+                Dashboard
+                </a>
+        @else
+
                 <a
                 class="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700"
                 href="{{ route('login') }}"
                 >
                 Log in
                 </a>
-
+                @if (Route::has('register'))
                 <a
                 class="inline-block rounded border border-gray-200 px-5 py-3 font-medium text-gray-700 shadow-sm transition-colors hover:bg-gray-50 hover:text-gray-900 dark:border-gray-700 dark:text-gray-200 dark:hover:bg-gray-800 dark:hover:text-white"
                 href="{{ route('register') }}"
                 >
                 register
                 </a>
-            </div>
+            @endif
+        @endauth
+
+        </div>
+     @endif
             </div>
 
             <svg
@@ -85,7 +103,7 @@
             </g>
             </svg>
         </div>
-        </section>
+    </section>
 
         <!-- <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
