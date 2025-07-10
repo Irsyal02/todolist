0968c6c10e255a19f337eb915a4f06759a6aa15d
AnonyHer <riers23@gmail.com>
2025-07-10 17:26:44 +0700

    Penambahan button pada dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <div class="flex justify-end">
+                        <a href="" class="bg-[#4f46e5] px-5 py-3 rounded-md">Make a list</a>
+                        
+                    </div>
                 </div>
             </div>
         </div>
